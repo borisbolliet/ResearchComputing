9081f9cee51782e045793632cea30c63b7806766
borisbolliet <boris.bolliet@gmail.com>
2024-09-05 12:05:12 +0100

    kernel name

--- docs/conf.py
+++ docs/conf.py
@@ -13,6 +13,8 @@
 # import os
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
+nbsphinx_kernel_name = 'python3'
+
 import sphinx_rtd_theme
 
 # -- Project information -----------------------------------------------------
