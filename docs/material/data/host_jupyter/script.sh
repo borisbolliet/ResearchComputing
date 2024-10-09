b5b9f53461c9c056598a93b824e130547546d816
borisbolliet <boris.bolliet@gmail.com>
2024-10-09 17:58:40 +0100

    update script.sh

--- docs/material/data/host_jupyter/script.sh
+++ docs/material/data/host_jupyter/script.sh
@@ -16,9 +16,12 @@ module purge
 # Load the require modules
 # module load jupyterlab/3
 module load rhel8/default-icl
-module load cfitsio-3.410-intel-17.0.4-4qrgkot intel/bundles/complib/2020.4 gsl-2.3-intel-17.0.4-ewbwjes fftw/intel/64/3.3.3
+# mexample: module load cfitsio-3.410-intel-17.0.4-4qrgkot intel/bundles/complib/2020.4 gsl-2.3-intel-17.0.4-ewbwjes fftw/intel/64/3.3.3
 
-source ~/.defaultEnv/bin/activate
+# source ~/.defaultEnv/bin/activate
+module load openmpi/4.1.5/intel/b42idtrx
+module load miniconda/3
+source $HOME/rds/$USER/envdir/base_311env/bin/activate
 
 which jupyter
 
